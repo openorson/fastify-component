f64bbe17ed32ae91d53b585454e3c6115f7a04f1
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-10-12 18:12:46 +0800

    feat: 玩命开发中...

--- packages/core/src/route/index.ts
+++ packages/core/src/route/index.ts
@@ -9,10 +9,45 @@ export function createRoute<
   return {};
 }
 
+// createRoute({
+//   method: "GET",
+//   query: {
+//     a: "boolean!",
+//   },
+//   handler(context) {
+//     context.query.a;
+//   },
+// });
+
 export function createRoutes<
-  Query extends NestedObjectValidatorExpression,
-  Body extends NestedObjectValidatorExpression,
-  Data extends NestedObjectValidatorExpression
->(routesOptions: Record<string, RouteOptions<Query, Body, Data>>): Route {
+  Paths extends string,
+  Options extends Record<Paths, RouteOptions<{}, {}, {}>>,
+  const RoutesOptions = {
+    [Path in Paths]: Options[Path] extends RouteOptions<infer Query, infer Body, infer Data> ? RouteOptions<Query, Body, Data> : never;
+  }
+>(routesOptions: RoutesOptions): Route {
   return {};
 }
+
+createRoutes({
+  a: {
+    method: "GET",
+    query: {
+      a: "boolean!",
+    },
+    body: {},
+    data: {},
+    handler(context) {
+      context.query;
+    },
+  },
+  b: {
+    method: "GET",
+    query: {
+      a: "boolean!",
+    },
+    body: {},
+    data: {},
+    handler(context) {},
+  },
+});
--- packages/core/src/types/route.ts
+++ packages/core/src/types/route.ts
@@ -9,9 +9,9 @@ export interface JSONResult<Data> {
 export type RouteResult<Data> = JSONResult<Data> | Promise<JSONResult<Data>> | void | Promise<void>;
 
 export interface RouteOptions<
-  Query extends NestedObjectValidatorExpression,
-  Body extends NestedObjectValidatorExpression,
-  Data extends NestedObjectValidatorExpression
+  Query extends NestedObjectValidatorExpression | void,
+  Body extends NestedObjectValidatorExpression | void,
+  Data extends NestedObjectValidatorExpression | void
 > {
   method: Uppercase<HTTPMethods>;
   query?: Query;
