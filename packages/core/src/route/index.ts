0f897765b00de8da8c65e92b9997f9028266eab7
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-10-13 17:33:22 +0800

    feat: 玩命开发中...

--- packages/core/src/route/index.ts
+++ packages/core/src/route/index.ts
@@ -8,3 +8,53 @@ export function createRoute<
 >(options: RouteOptions<Query, Body, Data>): Route {
   return {};
 }
+
+interface Type<T> {
+  label: T;
+  value: (arg: T) => void;
+}
+
+export function createRoutes<
+  Options extends Record<string, Type<unknown>>,
+  UnionOptions = {
+    [K in keyof Options]: {
+      [K2 in K]: Options[K2];
+    };
+  }[keyof Options]
+>(options: UnionOptions) {}
+
+createRoutes({
+  a: {
+    label: 1,
+    value(arg) {
+      console.log(arg);
+    },
+  },
+  b: {
+    label: "b",
+    value(arg) {
+      console.log(arg);
+    },
+  },
+});
+
+const opts = {
+  a: {
+    query: {
+      a: "boolean!",
+    },
+  },
+  b: {
+    query: {
+      b: 1,
+    },
+  },
+};
+
+type ToUnion<T extends {}> = {
+  [K in keyof T]: {
+    [K2 in K]: T[K2];
+  };
+}[keyof T];
+
+type Union = ToUnion<typeof opts>;
