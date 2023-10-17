c1c24118097b9bf234c1039c627592baedaa020c
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-10-17 16:15:38 +0800

    feat: 玩命开发中...

--- packages/core/src/route/index.ts
+++ packages/core/src/route/index.ts
@@ -1,59 +1,39 @@
-// import { NestedObjectValidatorExpression } from "expr-validator/dist/esm/validators/object";
-// import { RouteOptions, RoutesOptions } from "../types/route";
+import { NestedObjectValidatorExpression } from "expr-validator/dist/esm/validators/object";
+import { RouteOptions } from "../types/route";
 
-// export function createRoute<
-//   Query extends NestedObjectValidatorExpression,
-//   Body extends NestedObjectValidatorExpression,
-//   Data extends NestedObjectValidatorExpression
-// >(options: RouteOptions<Query, Body, Data>) {
-//   return {};
-// }
+export function createRoute<
+  Query extends NestedObjectValidatorExpression,
+  Body extends NestedObjectValidatorExpression,
+  Data extends NestedObjectValidatorExpression
+>(options: RouteOptions<Query, Body, Data>) {
+  return {};
+}
 
-// export function createRoutes<Options extends Record<string, NestedObjectValidatorExpression>>(options: RoutesOptions<Options>) {}
+export type RoutesOptions<Params extends Record<string, NestedObjectValidatorExpression>> = {
+  [Path in keyof Params]: RouteOptions<Params[Path], Params[Path], Params[Path]>;
+};
 
-// createRoutes({
-//   a: {
-//     method: "GET",
-//     query: {
-//       a: "boolean!",
-//     },
-//     handler(context) {
-//       context.query;
-//     },
-//   },
-//   b: {
-//     method: "GET",
-//     query: {
-//       a: "boolean!",
-//       b: "string!",
-//       c: "date!",
-//     },
-//     handler(context) {
-//       context.query;
-//     },
-//   },
-// });
+export function createRoutes<Options extends Record<string, NestedObjectValidatorExpression>>(options: RoutesOptions<Options>) {}
 
-// interface FooValue<T> {
-//   default: T;
-//   fn: (val: T) => any;
-// }
-
-// type Foo<T> = {
-//   [K in keyof T]: FooValue<T[K]>;
-// };
-
-// function asFoo<T>(foo: Foo<T>): Foo<T> {
-//   return foo;
-// }
-
-// asFoo({
-//   key1: {
-//     default: 1,
-//     fn(val) {},
-//   },
-//   key2: {
-//     default: "2",
-//     fn(val) {},
-//   },
-// });
+createRoutes({
+  a: {
+    method: "GET",
+    query: {
+      a: "boolean!",
+    },
+    handler(context) {
+      context.query;
+    },
+  },
+  b: {
+    method: "GET",
+    query: {
+      a: "boolean!",
+      b: "string!",
+      c: "date!",
+    },
+    handler(context) {
+      context.query;
+    },
+  },
+});
